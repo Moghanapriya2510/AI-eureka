54a1e6f10bf1dbe093e5e13797589bac31f93f31
Moghanapriya2510 <147739803+Moghanapriya2510@users.noreply.github.com>
2023-11-12 10:25:52 +0530

    code1.py

--- code1.py
+++ code1.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as p
+from matplotlib import pyplot as plt
 img=cv2.imread('flower.jpg',cv2.IMREAD_ANYCOLOR)
 cv2.imshow('Image',img)
 cv2.waitKey(0)
